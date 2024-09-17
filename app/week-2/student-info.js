ffebc1c13f75ab09c5418411944de61274cd6e0a
TerrellAW <Terrell.AlexisWilson@edu.sait.ca>
2024-09-17 12:08:11 -0600

    Switched HTML anchor to Next.js Link

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,7 +2,7 @@ export default function StudentInfo() {
     return (
         <div>
             <p>Name: Terrell Alexis-Wilson</p>
-            <a href="https://github.com/TerrellAW/cprg306-assignments">GitHub Repository</a>
+            <Link href="https://github.com/TerrellAW/cprg306-assignments">GitHub Repository</Link>
         </div>
     )
 }

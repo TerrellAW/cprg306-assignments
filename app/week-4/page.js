d713c5f9c620e6fcc6da1eee73e3e9293395ebb7
TerrellAW <Terrell.AlexisWilson@edu.sait.ca>
2024-09-28 14:32:34 -0600

    Fixed layout of page

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,9 +2,11 @@ import NewItem from "./new-item";
 
 export default function Page() {
     return (
-        <main className="bg-slate-800 min-h-screen  text-center">
+        <main className="bg-slate-800 min-h-screen flex flex-col items-center">
             <h1 className="text-2xl font-semibold text-sky-100 p-10">Week 4</h1>
-            <NewItem />
+            <div>
+                <NewItem />
+            </div>
         </main>
     );
 }

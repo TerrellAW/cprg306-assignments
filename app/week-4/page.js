74e811b40189b67e482d0d83a71c9e69abe2a883
TerrellAW <Terrell.AlexisWilson@edu.sait.ca>
2024-09-28 14:22:46 -0600

    Created counter component

---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <main className="bg-slate-800 min-h-screen  text-center">
+            <h1 className="text-2xl font-semibold text-sky-100 p-10">Week 4</h1>
+            <NewItem />
+        </main>
+    );
+}

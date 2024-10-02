c6804d232369606341dde4377768b92e4b7131db
TerrellAW <Terrell.AlexisWilson@edu.sait.ca>
2024-10-02 12:43:29 -0600

    Created Week 5

---
+++ app/week-5/new-item.js
@@ -0,0 +1,51 @@
+// Indicates to Next.js server that client-side JS is being used
+"use client";
+
+// Imports useState hook from React
+import { useState } from "react";
+
+// Component will count number of new items added by user
+export default function NewItem() {
+    const [quantity, setQuantity] = useState(1);
+
+    // Function to increment quantity of new items
+    function Increment() {
+        if (quantity < 20) {
+            setQuantity(quantity + 1);
+        }
+    }
+
+    // Function to decrement quantity of new items
+    function Decrement() {
+        if (quantity > 1) {
+            setQuantity(quantity - 1);
+        }
+    }
+
+    // Returns JSX elements for user interface
+    return (
+        <div className="flex items-center space-x-4">
+            <button 
+                onClick={Decrement} 
+                disabled={quantity === 1} 
+                className={
+                    `px-4 py-2 border rounded 
+                    ${quantity === 1 ? 'text-gray-500 border-gray-500' : 'text-sky-100 border-sky-100'}`
+                }
+            >
+                -
+            </button>
+            <span className="text-lg text-sky-100 w-4 text-center">{quantity}</span>
+            <button 
+                onClick={Increment} 
+                disabled={quantity === 20} 
+                className={
+                    `px-4 py-2 border rounded 
+                    ${quantity === 20 ? 'text-gray-500 border-gray-500' : 'text-sky-100 border-sky-100'}`
+                }
+            >
+                +
+            </button>
+        </div>
+    );
+}
---
+++ app/week-5/page.js
@@ -0,0 +1,12 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <main className="bg-slate-800 min-h-screen flex flex-col items-center">
+            <h1 className="text-2xl font-semibold text-sky-100 p-10">Week 4</h1>
+            <div>
+                <NewItem />
+            </div>
+        </main>
+    );
+}

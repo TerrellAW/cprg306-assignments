65a7396aa35775607e863480175c6f40fc74ebad
TerrellAW <Terrell.AlexisWilson@edu.sait.ca>
2024-10-02 16:58:21 -0600

    Started work on Form
    - Initialized variables
    - Created handleSubmit function
    - Started work on rendering the form, name field is in progress

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -6,7 +6,14 @@ import { useState } from "react";
 
 // Component will count number of new items added by user
 export default function NewItem() {
+    // Name
+    const [name, setName] = useState("");
+
+    // Quantity
     const [quantity, setQuantity] = useState(1);
+
+    // Category
+    const [category, setCategory] = useState("produce");
 
     // Function to increment quantity of new items
     function Increment() {
@@ -22,30 +29,56 @@ export default function NewItem() {
         }
     }
 
+    // Form submission handler
+    function handleSubmit(event) {
+        event.preventDefault();
+        item = { name, quantity, category };
+        console.log(item);
+        alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
+        setName("");
+        setQuantity(1);
+        setCategory("produce");
+    }
+
     // Returns JSX elements for user interface
     return (
-        <div className="flex items-center space-x-4">
-            <button 
-                onClick={Decrement} 
-                disabled={quantity === 1} 
-                className={
-                    `px-4 py-2 border rounded 
-                    ${quantity === 1 ? 'text-gray-500 border-gray-500' : 'text-sky-100 border-sky-100'}`
-                }
-            >
-                -
-            </button>
-            <span className="text-lg text-sky-100 w-4 text-center">{quantity}</span>
-            <button 
-                onClick={Increment} 
-                disabled={quantity === 20} 
-                className={
-                    `px-4 py-2 border rounded 
-                    ${quantity === 20 ? 'text-gray-500 border-gray-500' : 'text-sky-100 border-sky-100'}`
-                }
-            >
-                +
-            </button>
-        </div>
+        // Form element
+        <form className="flex flex-col items-center space-x-4">
+            {/* Name input field */}
+            <div className="mb-10">
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                    placeholder="Name"
+                    className="px-4 py-2 border rounded w-64"
+                />
+            </div>
+
+            {/* Item counter */}
+            <div className="flex items-center space-x-4">
+                <button 
+                    onClick={Decrement} 
+                    disabled={quantity === 1} 
+                    className={
+                        `px-4 py-2 border rounded 
+                        ${quantity === 1 ? 'text-gray-500 border-gray-500' : 'text-sky-100 border-sky-100'}`
+                    }
+                >
+                    -
+                </button>
+                <span className="text-lg text-sky-100 w-4 text-center">{quantity}</span>
+                <button 
+                    onClick={Increment} 
+                    disabled={quantity === 20} 
+                    className={
+                        `px-4 py-2 border rounded 
+                        ${quantity === 20 ? 'text-gray-500 border-gray-500' : 'text-sky-100 border-sky-100'}`
+                    }
+                >
+                    +
+                </button>
+            </div>
+        </form>
     );
 }

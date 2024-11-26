5536a35df618eab1872eb0c0d1f5e9b2f76167bf
ShadowNinja909 <Terrell.AlexisWilson@edu.sait.ca>
2024-11-26 15:08:17 -0700

    Service functions possibly functional

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -2,10 +2,27 @@
 import { db } from '../_utils/firebase.js';
 import { collection, getDocs, addDoc, query } from 'firebase/firestore';
 
-export async function getItems(userId) {
-    
-}
+export const getItems = async (userId) => {
+    try {
+        const shoppingItemsCollectionRef = collection(db, 'shopping-list', userId);
+        const shoppingItemsSnap = await getDocs(shoppingItemsCollectionRef);
 
-export function addItem() {
+        const shoppingItems = shoppingItemsSnap.docs.map((postDoc) => ({
+            userId: postDoc.userId,
+            ...postDoc.data()
+        }));
 
-}
+        return shoppingItems;
+    } catch (error) {
+        console.error('Error getting items:', error);
+    }
+};
+
+export const addItem = async (post) => {
+    try {
+        const shoppingItemsCollectionRef = collection(db, 'shopping-list', post);
+        return shoppingItemsCollectionRef.userId;
+    } catch (error) {
+        console.error('Error adding item:', error);
+    }
+};
